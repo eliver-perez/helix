d473cd1afc19bcae03581cb2922c1225e624b724
Eliver <eliverperezvillegas@gmail.com>
2026-07-02 12:46:00 -0600

    Inicio Productos

--- app/Core/config.php
+++ app/Core/config.php
@@ -2,6 +2,6 @@
 
 return [
     'name' => env('APP_NAME', 'Helix'),
-    'url'  => rtrim(env('APP_URL', 'http://localhost/helix/public'), '/'),
+    'url'  => rtrim(env('APP_URL', 'http://localhost:8080/helix/public'), '/'),
     'path' => dirname(__DIR__),
 ];
---
+++ public/products/index.php
@@ -0,0 +1,16 @@
+<?php
+	require_once __DIR__.'/../../app/Core/init.php';
+
+	$uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+	$segments = explode('/', $uri);
+
+	$index = array_search('products', $segments);
+
+	$action = $segments[$index + 1] ?? null;
+	$id     = $segments[$index + 1] ?? null;
+	$next   = $segments[$index + 2] ?? null;
+
+	// default
+	$view = APP_PATH . '/Views/products/index.php';
+
+	require APP_PATH . '/Views/layout/main.php';
